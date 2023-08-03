ae002d333cac2896e1de414b716dcae3dcb6ea24
祁恩泽 <sepveneto@qq.com>
2023-08-03 16:16:29 +0800

    chore: clear

--- src/components/face.js
+++ src/components/face.js
@@ -1,39 +1,19 @@
 import * as THREE from 'three'
 
-const vertexShader = `
-#include <skinning_pars_vertex>
-
-varying vec2 vUv;
-
-void main() {
-	#include <skinbase_vertex>
-	#include <begin_vertex>
-	#include <skinning_vertex>
-	#include <project_vertex>
-
-  vUv = uv;
-}
-`
 const fragmentParmas = `
 uniform vec2 mouth_offset;
 uniform sampler2D mouth_texture;
 `
 
 const fragmentStart = `
-vec4 c = diffuseColor * texture2D(mouth_texture, vMapUv + mouth_offset);
-vec4 mouthColor = c;
+vec4 mouthColor = diffuseColor * texture2D(mouth_texture, vMapUv + mouth_offset);
 `
 
 const fragmentEnd = `
 float alpha = diffuseColor.a;
 if (alpha == 0.0) {
   diffuseColor = mouthColor;
-} else {
-  diffuseColor.a = 1.0;
 }
-`
-
-const fragmentShader = `
 `
 
 export function createFace(material, uniforms = {} ) {
@@ -47,16 +27,9 @@ export function createFace(material, uniforms = {} ) {
     shaderList.splice(0, 0, '#include <face_mix_pars_fragment>')
     const index = shaderList.findIndex(item => item.includes('#include <map_fragment>'))
     shaderList.splice(index, 0, '#include <face_mix_fragment_start>')
+    // 不跟mouth的采样写在一起是因为那个时候纹理还没有进行采样，无法得到alpha值
     const i = shaderList.findIndex(item => item.includes('#include <alphamap_fragment>'))
     shaderList.splice(i + 1, 0, '#include <face_mix_fragment_end>')
-    // shaderList.splice(-1, 0, `
-    //   if (gl_FragColor.a > 0.0) {
-    //     gl_FragColor.a = 1.0;
-    //   }
-    // `)
-
     shader.fragmentShader = shaderList.join('\n')
-    console.log(shader.fragmentShader, shader)
   }
 }
-
--- src/useThree.js
+++ src/useThree.js
@@ -1,8 +1,7 @@
-import { onMounted, onUnmounted, shallowRef } from 'vue';
+import { onMounted, onUnmounted } from 'vue';
 import * as THREE from 'three';
 import createFloor from './components/floor'
 import { createFace } from './components/face';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { Hina } from './constant';
@@ -128,7 +127,6 @@ export function useThree(dom) {
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
   renderer.setSize( window.innerWidth, window.innerHeight );
-  // renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 
   const floor = createFloor()
   scene.add(floor)
@@ -145,9 +143,9 @@ export function useThree(dom) {
 	  requestAnimationFrame( animate );
     const delta = clock.getDelta()
     if (mixer) mixer.update(delta)
-    uniforms.mouth_offset.value = mouths[Math.floor(i / 6 % 16)]
-    ++i
-    if (i === 160) i = 0
+    // uniforms.mouth_offset.value = mouths[Math.floor(i / 6 % 16)]
+    // ++i
+    // if (i === 160) i = 0
 
 	  renderer.render( scene, camera );
   }
@@ -157,32 +155,15 @@ export function useThree(dom) {
     const controls = new OrbitControls(camera, renderer.domElement)
 
     const gltf = await loadGltf('/Hina_Original/Hina_Original.gltf')
-    // const gltf = await loadGltf('/notitle.glb')
     animations = gltf.animations
     const obj = gltf.scene
-    // const origin = obj.clone()
-    // origin.traverse(child => {
-    //   if (child.)
-    // })
-    // console.log(obj, origin)
+
     await mixMouth(obj)
-    // const obj = await loadObj('/fbx/Hina_Original/Hina_Original.fbx')
 
     scene.add(new THREE.AxesHelper(1))
 
 
-    // scene.add(origin)
-    // const m = obj.getObjectByName('Hina_Original_Body_3')
-    // m.material.transparent = true
-    // m.material.needsUpdate = true
-    // console.log(m)
-    // scene.add(m)
-    // setTimeout(() => {
-    //   m.material.transparent = true
-    //   m.material.needsUpdate = true
-    // })
     scene.add(obj)
-    // scene.add(gltf.temp)
     const target = obj.position.clone()
     camera.position.set(target.x, target.y, -(target.z + 0.03))
 
@@ -194,59 +175,17 @@ export function useThree(dom) {
   })
 }
 
-function loadObj(url) {
-  return new Promise(resolve => {
-    const loader = new FBXLoader()
-    loader.load(url, async (obj) => {
-      mixer = new THREE.AnimationMixer(obj)
-      mixer.clipAction(obj.animations[0]).play()
-
-      obj.traverse((child) => {
-        if (child.isMesh) {
-          // child.castShadow = true
-          // child.receiveShadow = true
-        }
-      })
-      resolve(obj)
-    })
-  })
-}
-
 function loadGltf(url) {
   return new Promise(resolve => {
     const loader = new GLTFLoader()
     loader.load(url, async (gltf) => {
       mixer = new THREE.AnimationMixer(gltf.scene)
+      mixer.clipAction(gltf.animations[Hina.EXS_CUTIN]).play()
 
-      // actions.filter(item => !!item).forEach((action, index) => {
-      //   settings[action] = false
-      //   gui.add(settings, action).onChange((val) => {
-      //     const animate = gltf.animations[index]
-      //     const clipAction = mixer.clipAction(animate)
-      //     if (val) {
-      //       clipAction.play()
-      //     } else {
-      //       clipAction.stop()
-      //     }
-      //   })
-      // })
-      // gui.add(settings, 'cafe_idle')
-      // gui.add(settings, 'cafe_reaction')
-      // gltf.animations.forEach(a => {
-      //   console.log(a)
-      //   gui.add(settings, a.name).onChange((val) => {
-      //     if (val) {
-      //       mixer.clipAction(gltf.animations.filter(i => i.name === a.name)[0])
-      //     }
-      //   })
-      // })
       gltf.scene.traverse( function ( object ) {
-        if (object?.material?.transparent || object.transparent) {
-          console.log(object, 'transparent')
-        }
         if ( object.isMesh ) {
-          // if (object.name !== 'Hina_Original_Body_4') object.visible = false
           object.castShadow = true;
+          object.material.vertexColors = false
         }
       })
 
@@ -265,130 +204,15 @@ function loadGltf(url) {
 }
 
 async function mixMouth(object) {
-   const obj = object.getObjectByName('Hina_Original_Body_3')
-      // console.log(material)
+  const obj = object.getObjectByName('Hina_Original_Body_3')
+  const mouthTex = await loadTexture('/Hina_Original/Hina_Mouth.png')
+  mouthTex.colorSpace = THREE.SRGBColorSpace
 
-      // material.onBeforeCompile = (shader) => {
-      //   const origin = shader.fragmentShader
-      //   console.log(origin)
-      //   const list = origin.split('\n')
-      //   const code = `
-      //   float alpha = gl_FragColor.a;
-      //   if (alpha > 0.0 ) {
-      //     gl_FragColor = vec4(gl_FragColor.rgb, 1.0);
-      //   } else {
-      //     gl_FragColor = vec4(gl_FragColor.rgb, 0.0);
-      //   }
-      //   `
-      //   list.splice(-1, 0, code)
-      //   shader.fragmentShader = list.join('\n')
-      // }
-
-      // material.needsUpdate = true
-      // material.alphaTest = 0
-
-      const mouthTex = await loadTexture('/Hina_Original/Hina_Mouth.png')
-      // mouthTex.encoding = THREE.sRGBEncoding
-      const eyeTex = await loadTexture('/Hina_Original/Hina_Original_EyeMouth.png')
-      eyeTex.colorSpace = THREE.SRGBColorSpace
-      mouthTex.colorSpace = THREE.SRGBColorSpace
-      // eyeTex.colorSpace = THREE.LinearSRGBColorSpace
-      // mouthTex.colorSpace = THREE.LinearSRGBColorSpace
-      // const eyeAlphaTex = await loadTexture('/Hina_Original/Hina_Original_EyeMouth_Alpha.png')
-      // // eyeTex.encoding = THREE.sRGBEncoding
-      // const eye = await loadImg('/Hina_Original/Hina_Original_EyeMouth.png')
-      // // const eye = await loadImg('/Hina_Original/Hina_Original_Weapon.png')
-      // const mouth = await loadImg('/Hina_Original/Hina_Mouth.png')
-
-      obj.material.map = eyeTex
-      // obj.material.alphaMap = eyeTex
-      // obj.material.depthWrite = false
-      obj.material.transparent = true
-      obj.material.needsUpdate = true
-      createFace(obj.material, uniforms)
-      m = obj.material
-      uniforms.mouth_texture.value = mouthTex
-
-      // const box = new THREE.Mesh(
-      //   new THREE.BoxGeometry(0.1, 0.1, 0.1),
-      //   new THREE.MeshStandardMaterial({
-      //     transparent: true,
-      //     map: eyeTex,
-      //     // alphaMap: eyeTex,
-      //   })
-      // )
-      // gltf.scene.visible = false
-      // gltf.temp = box
-      // gltf.temp.visible  = false
-
-      // const tex = new THREE.CanvasTexture(mixTex(eye, mouth))
-      // const texAlpha = new THREE.CanvasTexture(mixAlpha())
-      // tex.encoding = THREE.sRGBEncoding
-      // material.map = eyeTex
-      // material.alphaMap = eyeTex
-      // material.alphaMap = mouthTex
-      // tex.colorSpace = THREE.SRGBColorSpace
-      // material.blending = THREE.CustomBlending
-      // material.blendDst = THREE.DstAlphaFactor
-      // texAlpha.magFilter = THREE.LinearFilter
-      // material.map = tex
-      // material.alphaMap = texAlpha
-      // material.alphaMap = eyeAlphaTex
-      // material.alphaMap = alphaTex
-      // material.blending = THREE.NoBlending
-
-      // const faceMaterial = createFace(eyeTex, mouthTex)
-      // faceMaterial.map = eyeTex
-      // // const faceMaterial = material.clone()
-      // // faceMaterial.map = eyeTex
-      // // faceMaterial.needsUpdate = true
-      // faceMaterial.uniforms.texture1.value = eyeTex
-      // faceMaterial.uniforms.texture2.value = mouthTex
-      // faceMaterial.uniforms.offset.value.set(0.25, 0.75)
-
-      // console.log(obj.material, faceMaterial)
-      // obj.material = faceMaterial
-}
-function mixTex(eye, mouth) {
-  const canvas = document.createElement('canvas')
-  const ctx = canvas.getContext('2d')
-  canvas.style.position = 'absolute'
-  canvas.style.top = 0
-  canvas.style.left = 0
-
-  const { naturalWidth, naturalHeight } = mouth
-  canvas.width = naturalWidth
-  canvas.height = naturalHeight
-  // ctx.fillStyle = 'white'
-  // ctx.fillRect(0, 0, canvas.width, canvas.height)
-  // ctx.save()
-  ctx.drawImage(mouth, 0, 0)
-  ctx.scale(4, 4)
-  ctx.drawImage(eye, 0, 0)
-  // ctx.rotate(Math.PI/ 2)
-  document.body.appendChild(canvas)
-  return canvas
-}
-function mixAlpha() {
-  const canvas = document.createElement('canvas')
-  canvas.style.position = 'absolute'
-  canvas.style.top = '512px'
-  canvas.style.left = 0
-
-  const ctx = canvas.getContext('2d')
-  canvas.width = canvas.height = 512
-  ctx.fillStyle = 'black'
-  ctx.fillRect(0, 0, canvas.width, canvas.height)
-  document.body.appendChild(canvas)
-  return canvas
-}
-
-function loadImg(url) {
-  return new Promise(resolve => {
-    const img = document.createElement('img')
-    img.src = url
-    img.onload = () => resolve(img)
-  })
+  obj.material.transparent = true
+  createFace(obj.material, uniforms)
+  m = obj.material
+  uniforms.mouth_texture.value = mouthTex
+  obj.material.needsUpdate = true
 }
 
 function loadTexture(url) {
